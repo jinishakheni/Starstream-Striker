90db5a4b86c8850ad06beb7e1fc7bdeb952c4914
Jinisha Patel <jinishakheni@gmail.com>
2024-03-23 12:42:06 +0100

    Created basic intro screen

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
-window.onload(() => {
+window.onload = function () {
   // Start script execution after window gets loaded on the browser
-})
+  const startButton = document.querySelector("#start-button");
+
+  startButton.addEventListener("click", () => {
+    console.log("start button clicked")
+  });
+}
